c8008cc368ce7ab181469d8fbdb31dc3e6a7c2d9
Anandu Raj <ananduanjana22@gmail.com>
2022-02-09 13:37:24 +0530

    Second Commit

--- MyProject/src/main/java/MyPkg/AmazonBuy.java
+++ MyProject/src/main/java/MyPkg/AmazonBuy.java
@@ -14,41 +14,40 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class AmazonBuy {
-		WebDriver driver;
+	WebDriver driver;
 
-		@BeforeClass
-		public void driverInstance() {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			String URL = "https://www.amazon.in/";
-			driver.navigate().to(URL);
-		}
-		@Test(description = "Add a product to cart")
-		@Parameters("Item")
-		public void addToCart(String Item) throws InterruptedException {
-			Actions actions = new Actions(driver);
-			WebElement we = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-			actions.click(we).perform();
-			driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys(Item);
-			driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
-			driver.findElement(By.xpath("(//*[text()='Apple iPhone 12 (64GB) - White'])[3]")).click();
-			String handle = driver.getWindowHandle();
-			Set<String> handles = driver.getWindowHandles();
-			handles.remove(handle);
-			String newhandle = handles.iterator().next();
-			driver.switchTo().window(newhandle);
-			JavascriptExecutor js = (JavascriptExecutor) driver;
-			WebElement we1 = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
-			js.executeScript("arguments[0].scrollIntoView();", we1);
-			we1.click();
-			Thread.sleep(5000);
-		}
-
-		@AfterClass
-		public void close() {
-			driver.quit();
-		}
+	@BeforeClass
+	public void driverInstance() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		String URL = "https://www.amazon.in/";
+		driver.navigate().to(URL);
 	}
 
+	@Test(description = "Add a product to cart")
+	@Parameters("Item")
+	public void addToCart(String Item) throws InterruptedException {
+		Actions actions = new Actions(driver);
+		WebElement we = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		actions.click(we).perform();
+		driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys(Item);
+		driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+		driver.findElement(By.xpath("(//*[text()='Apple iPhone 12 (64GB) - White'])[3]")).click();
+		String handle = driver.getWindowHandle();
+		Set<String> handles = driver.getWindowHandles();
+		handles.remove(handle);
+		String newhandle = handles.iterator().next();
+		driver.switchTo().window(newhandle);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		WebElement we1 = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
+		js.executeScript("arguments[0].scrollIntoView();", we1);
+		we1.click();
+		Thread.sleep(5000);
+	}
+
+	@AfterClass
+	public void close() {
+		driver.quit();
+	}
 }
