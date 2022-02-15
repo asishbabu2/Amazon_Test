3040d7bcae4b5668e2417cdf23e5aa980ea8c1ef
Asish Babu <99235097+asishbabu2@users.noreply.github.com>
2022-02-15 16:34:52 +0530

    Update AmazonLogin.java

--- MyProject/src/main/java/MyPkg/AmazonLogin.java
+++ MyProject/src/main/java/MyPkg/AmazonLogin.java
@@ -5,24 +5,31 @@ import org.openqa.selenium.WebDriver;
 
 public class AmazonLogin {
 	WebDriver driver;
-
+        WebDriverWait wait;
+	
 	public AmazonLogin(WebDriver driver) {
 		this.driver = driver;
 	}
 	By signinButton = By.xpath("(//*[text()='Hello, Sign in'])[1]");
-	By signin = By.xpath("(//*[text()='Sign in'])[1]");
 	By emailID = By.xpath("//input[@name='email']");
 	By continueButton = By.xpath("//input[@id='continue']");
 	By password = By.xpath("//input[@name='password']");
 	By submit = By.xpath("//input[@id='signInSubmit']");
 
 	public void Email(String email) {
-		driver.findElement(signinButton).click();
-		driver.findElement(emailID).sendKeys(email);
-		driver.findElement(continueButton).click();
+		wait = new WebDriverWait(driver, 20);
+		WebElement we1 = wait.until(ExpectedConditions.visibilityOfElementLocated(signinButton));
+		we1.click();
+		WebElement we2 = wait.until(ExpectedConditions.visibilityOfElementLocated(emailID));
+		we2.sendKeys(email);
+		WebElement we3 = wait.until(ExpectedConditions.visibilityOfElementLocated(continueButton));
+		we3.click();
 	}
 	public void password(String Password) {
-		driver.findElement(password).sendKeys(Password);
-		driver.findElement(submit).click();
+		wait = new WebDriverWait(driver, 20);
+		WebElement we2 = wait.until(ExpectedConditions.visibilityOfElementLocated(password));
+		we2.sendKeys(Password);
+		WebElement we3 = wait.until(ExpectedConditions.visibilityOfElementLocated(submit));
+		we3.click();
 	}
 }
